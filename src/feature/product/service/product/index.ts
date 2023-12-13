00eabf6550405bddb61aac856905db28cbea89b1
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-14 00:50:19 +0100

    Update product service

--- src/feature/product/service/product/index.ts
+++ src/feature/product/service/product/index.ts
@@ -2,26 +2,31 @@ import { Product } from "../../entity/Product.entity";
 import { ProductRepository } from "../../repository/product.in-memory.repository";
 
 export const ProductService = {
-  async getProductByBarCode(barCode: string): Promise<Product | null> {
-    return ProductRepository.getProductByBarCode(barCode);
-  },
-
-  async createProduct(productData: {
-    name: string;
-    barCode: string;
-  }): Promise<Product> {
-    return ProductRepository.insertProduct({
-      barCode: productData.barCode,
-      name: productData.name,
-      quantity: 1,
-    });
-  },
-
-  async increaseQuantity(
-    product: Product,
-    quantityToIncrease: number
-  ): Promise<Product> {
-    const newQuantity = product.quantity + quantityToIncrease;
-    return ProductRepository.updateProductQuantity(product.id, newQuantity);
-  },
+  getProductByBarCode,
+  createProduct,
+  updateProductQuantity,
 };
+
+async function getProductByBarCode(barCode: string): Promise<Product | null> {
+  return ProductRepository.getProductByBarCode(barCode);
+}
+
+export type ProductData = {
+  name: string;
+  barCode: string;
+};
+
+async function createProduct(productData: ProductData): Promise<Product> {
+  return ProductRepository.insertProduct({
+    barCode: productData.barCode,
+    name: productData.name,
+    quantity: 1,
+  });
+}
+
+async function updateProductQuantity(
+  productId: string,
+  newQuantity: number
+): Promise<Product> {
+  return ProductRepository.updateProductQuantity(productId, newQuantity);
+}
