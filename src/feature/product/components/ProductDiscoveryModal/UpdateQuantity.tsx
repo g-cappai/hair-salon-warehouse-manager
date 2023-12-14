a01f8e085d85457b38248740af63d35feca500fa
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-14 19:38:28 +0100

    Create temp UpdateQuantity

---
+++ src/feature/product/components/ProductDiscoveryModal/UpdateQuantity.tsx
@@ -0,0 +1,36 @@
+import { Button, View } from "react-native";
+import { useUpdateProductQuantity } from "../../hooks/useUpdateProductQuantity";
+
+type Props = {
+  productId: string;
+  currentQuantity: number;
+  onCancel: () => void;
+};
+
+export function UpdateQuantity({
+  productId,
+  currentQuantity,
+  onCancel,
+}: Props) {
+  const { mutate: updateProductQuantity } = useUpdateProductQuantity();
+
+  const handleIncreaseByOne = () => {
+    updateProductQuantity({ productId, newQuantity: currentQuantity + 1 });
+  };
+
+  const handleDecreaseByOne = () => {
+    updateProductQuantity({ productId, newQuantity: currentQuantity - 1 });
+  };
+
+  const handleCancel = () => {
+    onCancel();
+  };
+
+  return (
+    <View>
+      <Button title="Increase by one" onPress={handleIncreaseByOne} />
+      <Button title="Decrease by one" onPress={handleDecreaseByOne} />
+      <Button title="Cancel" onPress={handleCancel} />
+    </View>
+  );
+}
