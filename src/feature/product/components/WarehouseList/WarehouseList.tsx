f37faf58d4128a95992b6a48157a9bc4af739f49
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-15 20:22:53 +0100

    Create temp warehouseList

---
+++ src/feature/product/components/WarehouseList/WarehouseList.tsx
@@ -0,0 +1,20 @@
+import { Text } from "react-native";
+import { useGetProducts } from "src/feature/product/hooks/useGetProducts";
+import { List } from "src/feature/ui/components";
+export function WarehouseList() {
+  const { data: products } = useGetProducts();
+  /*
+  TODO:
+  - I want to press a list item to display its informations
+  - I need a updateProduct to update the whole product informations
+  - I want a button to delete the item
+  */
+  return (
+    <List
+      data={products}
+      renderItem={({ item }) => (
+        <Text>{`Name: ${item.name} Quantity: ${item.quantity}`}</Text>
+      )}
+    />
+  );
+}
