f919160574f32cfe50d29ff3a14e0711e6653e3a
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-15 01:15:45 +0100

    Redirect from "/"

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { useState } from "react";
-import { Scanner } from "src/feature/scanner/components";
-import { ProductDiscoveryModal } from "src/feature/product/components";
+import { Redirect } from "expo-router";
 
-export default function Home() {
-  // barCode is scanned
-  const [barCode, setBarCode] = useState("");
-  // the scanning is paused
-  const isScanningPaused = !!barCode;
-
-  const handleCloseModal = () => {
-    setBarCode("");
-  };
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Scanner
-        isPaused={isScanningPaused}
-        isActive={true}
-        onScan={setBarCode}
-      />
-      <ProductDiscoveryModal
-        isOpen={!!barCode}
-        barCode={barCode}
-        onClose={handleCloseModal}
-      />
-    </View>
-  );
+export default function HomePage() {
+  return <Redirect href={"/scanner"} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
