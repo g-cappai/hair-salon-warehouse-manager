35ea6684591bc14022441280224fe7006a10a5af
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-15 01:15:29 +0100

    Update navigation tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,20 @@
 import { Tabs } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function App() {
+export default function Layout() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Tabs>
-        <Tabs.Screen
-          name="index"
-          options={{ headerShown: false }}
-          initialParams={{ a: "", b: "" }}
-        />
+      <Tabs
+        screenOptions={{
+          tabBarIconStyle: { display: "none" },
+          tabBarLabelPosition: "beside-icon",
+        }}
+      >
+        <Tabs.Screen name="index" options={{ href: null }} />
+        <Tabs.Screen name="warehouse" options={{ title: "Magazzino" }} />
+        <Tabs.Screen name="scanner" options={{ title: "Scanner" }} />
       </Tabs>
     </QueryClientProvider>
   );
