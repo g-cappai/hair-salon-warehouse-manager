e2e12f2f1a8b0b697719b85111cef1c064ce42ac
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-16 17:39:37 +0100

    Finish temp warehouse list

--- src/features/product/components/WarehouseList/WarehouseList.tsx
+++ src/features/product/components/WarehouseList/WarehouseList.tsx
@@ -1,20 +1,25 @@
-import { Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import { List } from "src/features/ui/components";
-import { useGetProducts } from "../../data-access/hooks/useGetProducts";
+import { Link } from "@features/navigation/components";
+import { useGetProducts } from "@features/product/data-access/hooks/useGetProducts";
 
 export function WarehouseList() {
   const { data: products } = useGetProducts();
-  /*
-  TODO:
-  - I want to press a list item to display its informations
-  - I need a updateProduct to update the whole product informations
-  - I want a button to delete the item
-  */
+
   return (
     <List
       data={products}
       renderItem={({ item }) => (
-        <Text>{`Name: ${item.name} Quantity: ${item.quantity}`}</Text>
+        <Link
+          href={{
+            pathname: "/warehouse/product/[id]",
+            params: { id: item.id },
+          }}
+        >
+          <Pressable>
+            <Text>{`Name: ${item.name} Quantity: ${item.quantity}`}</Text>
+          </Pressable>
+        </Link>
       )}
     />
   );
