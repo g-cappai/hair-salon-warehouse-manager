4fe42a2afdf7f0c73d1be229a0ea200bf237bbeb
Gianluca Cappai <gcappai.dev@@gmail.com>
2023-12-24 01:28:04 +0100

    Update temp creation page

--- src/features/core/components/ProductDiscoveryModal/CreateProductForm.tsx
+++ src/features/core/components/ProductDiscoveryModal/CreateProductForm.tsx
@@ -1,26 +1,106 @@
-import { Button, TextInput, View } from "react-native";
+import { Button, View } from "react-native";
 import { useState } from "react";
-import { useCreateProduct } from "../../data-access/hooks/useCreateProduct";
+import { Input } from "@features/ui/components";
+import { Select } from "@features/ui/components";
+import { useCreateProduct } from "@features/core/data-access/hooks/product";
+import { Brand } from "@features/core/entity/Product.entity";
+import { useGetCategories } from "@features/core/data-access/hooks/category/useGetCategories";
+import { useGetCategoryDetails } from "@features/core/data-access/hooks/category";
 
 type Props = {
   barCode: string;
   onSubmit: (submittedValues: { name: string; barCode: string }) => void;
 };
 
-export function CreateProductForm({ barCode, onSubmit }: Props) {
-  const { mutate: createProduct } = useCreateProduct();
+const BRANDS: Brand[] = [
+  { id: "0", name: "Alfaparf" },
+  { id: "1", name: "Evolution" },
+];
 
-  const [productName, setProductName] = useState("");
+export function CreateProductForm({ barCode, onSubmit }: Props) {
+  const [formData, setFormData] = useState({
+    barCode,
+    quantity: 1,
+    brandId: "",
+    categoryId: "",
+    details: [] as { categoryDetailId: string; value: string }[],
+  });
+  const { mutate: createProduct } = useCreateProduct();
+  const { data: categories } = useGetCategories();
+  const { data: categoryDetails } = useGetCategoryDetails({
+    categoryId: formData.categoryId,
+  });
 
   const handleSubmit = () => {
-    //TODO We need every product property
-    createProduct({ barCode, category: "bleach", brand: "Test", quantity: 1 });
-    onSubmit({ name: productName, barCode });
+    createProduct({
+      barCode,
+      brandId: formData.brandId,
+      categoryId: formData.categoryId,
+      quantity: formData.quantity,
+      details: formData.details,
+    });
+    onSubmit({ name: "productName", barCode });
   };
 
   return (
     <View>
-      <TextInput placeholder="Name" onChangeText={setProductName} />
+      <Input
+        placeholder="Barcode"
+        value={formData.barCode}
+        onChange={(value) => setFormData((c) => ({ ...c, barCode: value }))}
+      />
+      <Input
+        placeholder="Quantity"
+        type="NUMERIC"
+        value={formData.quantity.toString()}
+        onChange={(value) => setFormData((c) => ({ ...c, quantity: +value }))}
+      />
+      <Select
+        placeholder="Brand"
+        data={BRANDS.map((b) => ({ label: b.name, value: b.id }))}
+        selectedValue={formData.brandId}
+        onChange={(value) =>
+          setFormData((c) => ({ ...c, brandId: value as string }))
+        }
+      />
+      <Select
+        placeholder="Category"
+        data={categories?.map((c) => ({ label: c.name, value: c.id })) || []}
+        selectedValue={formData.categoryId}
+        onChange={(value) => {
+          setFormData((c) => ({ ...c, categoryId: value as string }));
+        }}
+      />
+
+      {categoryDetails?.map((categoryDetail) => {
+        if (categoryDetail.type === "string") {
+          return (
+            <Input
+              key={categoryDetail.id}
+              placeholder={categoryDetail.label}
+              value={
+                formData.details.find(
+                  (formDataDetail) =>
+                    formDataDetail.categoryDetailId === categoryDetail.id
+                )?.value || ""
+              }
+              onChange={(value) =>
+                setFormData((currentFormData) => ({
+                  ...currentFormData,
+                  details: [
+                    ...currentFormData.details.filter(
+                      (currentFormDataDetail) =>
+                        currentFormDataDetail.categoryDetailId !==
+                        categoryDetail.id
+                    ),
+                    { categoryDetailId: categoryDetail.id, value },
+                  ],
+                }))
+              }
+            />
+          );
+        }
+      })}
       <Button title="CREATE" onPress={handleSubmit} />
     </View>
   );
