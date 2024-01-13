9a95d41d1b30d8b21f09e9e95379ff6e33377ad6
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-01-13 16:13:14 +0100

    Create Brand repository

---
+++ src/features/core/data-access/repository/brand.in-memory.repository.ts
@@ -0,0 +1,34 @@
+import { Brand } from "@features/core/entity/Product.entity";
+
+export type BrandModel = {
+  id: string;
+  name: string;
+};
+
+const brands = new Map<string, BrandModel>(
+  [
+    { id: "0", name: "Alfaparf" },
+    { id: "1", name: "Evolution" },
+  ].map((v) => [v.id, v])
+);
+
+export async function getBrandById(id: string): Promise<Brand | undefined> {
+  return brands.get(id);
+}
+
+export async function getBrands(): Promise<Brand[]> {
+  return Array.from(brands.values());
+}
+
+export async function getBrandsByIds(ids: string[]): Promise<Brand[]> {
+  const brandsByIds: Brand[] = [];
+  for (const id of ids) {
+    const brand = await getBrandById(id);
+    if (brand) {
+      brandsByIds.push(brand);
+    }
+  }
+  return brandsByIds;
+}
+
+export const BrandRepository = { getBrandById, getBrands, getBrandsByIds };
