82e33e10a3a16842cd8f66b5b454283ab087e689
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-01-13 18:29:27 +0100

    Update CategoryService

--- src/features/core/data-access/service/CategoryService.ts
+++ src/features/core/data-access/service/CategoryService.ts
@@ -1,9 +1,13 @@
-import { Category, CategoryDetail } from "../../entity/Product.entity";
+import {
+  Category,
+  CategoryDetail,
+} from "@features/core/entity/Category.entity";
 import { CategoryRepository } from "../repository/category.in-memory.repository";
 
 export const CategoryService = {
   getCategories,
   getCategoryDetails,
+  getCategoryById, // Added getCategoryById function
 };
 
 async function getCategories(): Promise<Category[]> {
@@ -14,4 +18,8 @@ async function getCategoryDetails(
   categoryId: string
 ): Promise<CategoryDetail[]> {
   return CategoryRepository.getCategoryDetails(categoryId);
+}
+
+async function getCategoryById(categoryId: string): Promise<Category | null> {
+  return CategoryRepository.getCategoryById(categoryId);
 }
