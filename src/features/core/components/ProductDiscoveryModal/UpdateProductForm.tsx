d627aae192ac22bb8b015addbdd188583e915a3f
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-01-21 20:00:50 +0100

    Update UpdateForm

--- src/features/core/components/ProductDiscoveryModal/UpdateProductForm.tsx
+++ src/features/core/components/ProductDiscoveryModal/UpdateProductForm.tsx
@@ -1,5 +1,7 @@
 import { useUpdateProductQuantity } from "@features/core/data-access/hooks/product";
-import { Button, View } from "react-native";
+import { Link } from "@features/navigation/components";
+import { Text } from "@features/ui/components";
+import { Button, StyleSheet, View } from "react-native";
 type Props = {
   productId: string;
   currentQuantity: number;
@@ -33,9 +35,29 @@ export function UpdateProductForm({
 
   return (
     <View>
-      <Button title="Increase by one" onPress={handleIncreaseByOne} />
-      <Button title="Decrease by one" onPress={handleDecreaseByOne} />
+      <View style={styles.header}>
+        <Text variant="title">Prodotto trovato!</Text>
+        <Text variant="body">
+          Il prodotto si trova in magazzino. Cosa vuoi fare?
+        </Text>
+      </View>
+      <Button title="+1" onPress={handleIncreaseByOne} />
+      <Button title="-1" onPress={handleDecreaseByOne} />
+      <Link
+        href={{
+          pathname: "/warehouse/product/[id]",
+          params: { id: productId },
+        }}
+      >
+        <Button title="Vai al prodotto" onPress={onCancel} />
+      </Link>
       <Button title="Cancel" onPress={handleCancel} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    paddingBottom: 10,
+  },
+});
