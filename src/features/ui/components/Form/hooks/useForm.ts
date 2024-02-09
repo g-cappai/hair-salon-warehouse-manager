cf915dc47c9f6e2d34bf9aa9b055662ebf25bf77
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-02-09 01:55:50 +0100

    Update useForm

--- src/features/ui/components/Form/hooks/useForm.ts
+++ src/features/ui/components/Form/hooks/useForm.ts
@@ -1,12 +1,21 @@
-import { DefaultValues, useForm as useReactHookForm } from "react-hook-form";
+import {
+  DefaultValues,
+  Resolver,
+  useForm as useReactHookForm,
+} from "react-hook-form";
 
-type UseFormParams<T> = {
+type UseFormParams<T extends object> = {
   initialValues: T;
+  resolver: Resolver<T>;
 };
 
-export function useForm<T extends object>({ initialValues }: UseFormParams<T>) {
-  const { watch, handleSubmit, setValue } = useReactHookForm({
+export function useForm<T extends object>({
+  initialValues,
+  resolver,
+}: UseFormParams<T>) {
+  const { watch, handleSubmit, setValue } = useReactHookForm<T>({
     defaultValues: initialValues as DefaultValues<T>,
+    resolver,
   });
 
   return {
