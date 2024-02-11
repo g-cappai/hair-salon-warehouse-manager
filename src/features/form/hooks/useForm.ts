0846d7ddb81e2adc8f200c7ce0b92890439a1760
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-02-11 12:47:50 +0100

    Add setTouched to modal

---
+++ src/features/form/hooks/useForm.ts
@@ -0,0 +1,53 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  PathValue,
+  useForm as useReactHookForm,
+} from "react-hook-form";
+import { ObjectSchema, AnyObject } from "yup";
+
+type UseFormParams<T extends FieldValues> = {
+  /**
+   *  Accepts a yup schema
+   */
+  schema: ObjectSchema<T>;
+  initialValues: DefaultValues<T>;
+};
+
+export function useForm<T extends AnyObject>({
+  initialValues,
+  schema,
+}: UseFormParams<T>) {
+  const {
+    watch,
+    handleSubmit,
+    setValue: setFieldValue,
+    formState,
+  } = useReactHookForm({
+    /** @ts-expect-error: yupResolver will create a Resover<MakeOptionalKeys<T>> type.*/
+    resolver: yupResolver(schema),
+    defaultValues: initialValues,
+    mode: "all",
+  });
+
+  function setValue(name: Path<T>, value: PathValue<T, Path<T>>) {
+    setFieldValue(name, value, { shouldDirty: true, shouldValidate: true });
+  }
+
+  function setTouched(name: Path<T>) {
+    return () =>
+      setFieldValue(name, watch()[name], {
+        shouldTouch: true,
+      });
+  }
+
+  return {
+    handleSubmit,
+    setValue,
+    setTouched,
+    values: watch(),
+    formState,
+  };
+}
