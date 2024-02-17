9f57145e008f279a2c337955de6b76d171feccbf
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-02-17 18:37:01 +0100

    Add modal toggling

--- app/scanner.tsx
+++ app/scanner.tsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { useFocusEffect } from "expo-router";
 import { Scanner } from "@features/scanner/components";
 import { ScannedProductDialog } from "@features/core/components";
+import { CreateProductModal } from "@features/core/components/CreateProductModal";
 
 export default function ScannerPage() {
   const [isScannerActive, setIsScannerActive] = useState(true);
   const [barCode, setBarCode] = useState("");
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(true);
   const isScanningPaused = !!barCode;
 
   useFocusEffect(() => {
@@ -32,7 +34,13 @@ export default function ScannerPage() {
         <ScannedProductDialog
           isOpen={!!barCode}
           barCode={barCode}
+          onCreate={() => setIsCreateModalOpen(true)}
           onClose={handleCloseModal}
+        />
+        <CreateProductModal
+          barCode={barCode}
+          isOpen={isCreateModalOpen}
+          onClose={() => setIsCreateModalOpen(false)}
         />
       </View>
     </View>
