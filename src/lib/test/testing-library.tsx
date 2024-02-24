dc9937bce4fb6000a3762b8dbb3c1815e603bc10
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-02-25 00:30:20 +0100

    Setup testing-library

--- src/lib/test/testing-library.tsx
+++ src/lib/test/testing-library.tsx
@@ -1,17 +1,24 @@
 import { render } from "@testing-library/react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 
-const AllTheProviders = ({ children }: PropsWithChildren) => {
-  const queryClient = new QueryClient();
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 0,
+    },
+  },
+});
+export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={null}>{children}</Suspense>
+    </QueryClientProvider>
   );
 };
 
 const customRender: typeof render = (ui, options) =>
-  render(ui, { wrapper: AllTheProviders, ...options });
+  render(ui, { wrapper: Providers, ...options });
 
 export * from "@testing-library/react-native";
 
