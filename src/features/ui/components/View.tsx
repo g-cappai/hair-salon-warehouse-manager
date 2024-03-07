03546796a97e553ae84070f5612c9d3c9da7cb84
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-03-07 15:50:17 +0100

    Pass View props to View custom

--- src/features/ui/components/View.tsx
+++ src/features/ui/components/View.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren } from "react";
-import { StyleProp, ViewStyle } from "react-native";
+import { ViewProps } from "react-native";
 import { View as UILibView } from "react-native-ui-lib";
 
-type Props = { style?: StyleProp<ViewStyle> } & PropsWithChildren;
+type Props = ViewProps & PropsWithChildren;
 
-export function View({ style, children }: Props) {
-  return <UILibView style={style}>{children}</UILibView>;
+export function View({ children, ...props }: Props) {
+  return <UILibView {...props}>{children}</UILibView>;
 }
