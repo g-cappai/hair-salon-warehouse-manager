bd085f9f1c36d097ece2af2f1977ad36e620f1dc
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-03-08 12:55:39 +0100

    Fix repositories export

--- src/features/core/data-access/repository/index.ts
+++ src/features/core/data-access/repository/index.ts
@@ -1,3 +1,3 @@
-export { ProductRepository } from "../repository/product.in-memory.repository";
-export { CategoryRepository } from "../repository/category.in-memory.repository";
-export { BrandRepository } from "../repository/brand.in-memory.repository";
+export { ProductRepository } from "./product.in-memory.repository";
+export { CategoryRepository } from "./category.in-memory.repository";
+export { BrandRepository } from "./brand.in-memory.repository";
