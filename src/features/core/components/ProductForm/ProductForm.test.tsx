17818c9959856559abe9eb6910ea5f8d1bd3bc68
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-03-08 18:26:05 +0100

    Update ProductForm test to handle existing product case

--- src/features/core/components/ProductForm/ProductForm.test.tsx
+++ src/features/core/components/ProductForm/ProductForm.test.tsx
@@ -7,13 +7,17 @@ import {
 } from "@lib/test/testing-library";
 import { ProductForm } from "./ProductForm";
 import { useProductForm } from "./hooks/useProductForm";
-import { __seedInMemory } from "@features/core/data-access/repository/in-memory.data";
+import {
+  __seedInMemory,
+  __setProducts,
+} from "@features/core/data-access/repository/in-memory.data";
 
 jest.mock("../../data-access/repository");
 
 describe("ProductForm", () => {
-  it("should render category details after selecting the category", async () => {
+  it("should render category details after selecting the category when product is not present", async () => {
     __seedInMemory();
+    __setProducts([]);
 
     const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
       wrapper: Providers,
@@ -34,5 +38,23 @@ describe("ProductForm", () => {
     rerender(<ProductForm form={result.current.form} />);
 
     expect(getByText("cat.det.3.label")).toBeOnTheScreen();
+  });
+
+  it("should populate the form with the product data if it is already present", async () => {
+    __seedInMemory();
+
+    const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
+      wrapper: Providers,
+    });
+
+    await waitFor(() => {
+      result.current.form;
+    });
+
+    const { getByDisplayValue } = render(
+      <ProductForm form={result.current.form} />
+    );
+
+    expect(getByDisplayValue("cat.det.1.val")).toBeOnTheScreen();
   });
 });
