7350bc0a5f71f48c53d76fdb5372078d2c14db23
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-03-08 20:34:57 +0100

    Add product form update test case

--- src/features/core/components/ProductForm/ProductForm.test.tsx
+++ src/features/core/components/ProductForm/ProductForm.test.tsx
@@ -57,4 +57,28 @@ describe("ProductForm", () => {
 
     expect(getByDisplayValue("cat.det.1.val")).toBeOnTheScreen();
   });
+
+  it("should reset details data if category cahnges", async () => {
+    __seedInMemory();
+
+    const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
+      wrapper: Providers,
+    });
+
+    await waitFor(() => {
+      result.current.form;
+    });
+
+    const { getByTestId, getByText, rerender } = render(
+      <ProductForm form={result.current.form} />
+    );
+
+    await waitFor(() => fireEvent.press(getByTestId("category-select")));
+
+    await waitFor(() => fireEvent.press(getByText("cat.2")));
+
+    rerender(<ProductForm form={result.current.form} />);
+
+    expect(getByText("cat.det.3.label")).toBeOnTheScreen();
+  });
 });
