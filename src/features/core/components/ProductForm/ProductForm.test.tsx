7008e21f0edf13d3aa948ab2b296a6436268fbc1
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-03-09 16:42:01 +0100

    Update test

--- src/features/core/components/ProductForm/ProductForm.test.tsx
+++ src/features/core/components/ProductForm/ProductForm.test.tsx
@@ -1,25 +1,25 @@
 import {
   Providers,
   fireEvent,
+  invalidateTestQueries,
   render,
   renderHook,
   waitFor,
 } from "@lib/test/testing-library";
 import { ProductForm } from "./ProductForm";
 import { useProductForm } from "./hooks/useProductForm";
-import {
-  __seedInMemory,
-  __setProducts,
-} from "@features/core/data-access/repository/in-memory.data";
+import { __seedInMemory } from "@features/core/data-access/repository/in-memory.data";
 
 jest.mock("../../data-access/repository");
 
 describe("ProductForm", () => {
-  it("should render category details after selecting the category when product is not present", async () => {
+  beforeAll(() => {
     __seedInMemory();
-    __setProducts([]);
+    invalidateTestQueries();
+  });
 
-    const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
+  it("should render category details after selecting the category when product is not present", async () => {
+    const { result } = renderHook(() => useProductForm({ barCode: "53463" }), {
       wrapper: Providers,
     });
 
@@ -50,8 +50,6 @@ describe("ProductForm", () => {
   }, 10000);
 
   it("should populate the form with the product data if it is already present", async () => {
-    __seedInMemory();
-
     const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
       wrapper: Providers,
     });
@@ -69,9 +67,7 @@ describe("ProductForm", () => {
     );
   });
 
-  it("should reset details data if category cahnges", async () => {
-    __seedInMemory();
-
+  it("should reset details data if category changes", async () => {
     const { result } = renderHook(() => useProductForm({ barCode: "1234" }), {
       wrapper: Providers,
     });
@@ -89,9 +85,18 @@ describe("ProductForm", () => {
     await waitFor(() => fireEvent.press(getByText("cat.2")));
 
     rerender(<ProductForm form={result.current.form} />);
-    await waitFor(() => {
-      expect(getByDisplayValue("brand.1")).toBeOnTheScreen();
+
+    await waitFor(async () => {
+      const brand = await getByDisplayValue("brand.1", {
+        includeHiddenElements: true,
+      });
+      const category = await getByDisplayValue("cat.1", {
+        includeHiddenElements: true,
+      });
+
+      expect(brand).toBeOnTheScreen();
+      expect(category).toBeOnTheScreen();
       expect(getByText("cat.det.3.label")).toBeOnTheScreen();
     });
-  });
+  }, 10000);
 });
